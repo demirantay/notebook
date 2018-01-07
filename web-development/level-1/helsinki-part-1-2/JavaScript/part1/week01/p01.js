ec1a50fd81740d9d74c7e447516344c8e4cbfe91
Demir Antay <demir99antay@gmail.com>
2018-01-07 15:45:10 +0200

    added more js

--- web-development/level-1/helsinki-part-1-2/JavaScript/part1/week01/p01.js
+++ web-development/level-1/helsinki-part-1-2/JavaScript/part1/week01/p01.js
@@ -33,3 +33,26 @@ console.log('In a nutshell: ');
 console.log(chickens);
 console.log(bacon);
 console.log(tractors);
+
+//line
+console.log('=====================');
+
+//exersice 5
+
+var days_in_a_year = 365;
+var hours_in_a_year = days_in_a_year * 24;
+var seconds_in_a_year = hours_in_a_year * 60;
+
+console.log('Seconds in a year: ' + seconds_in_a_year);
+
+//line
+console.log('=====================');
+
+//exersice 6
+var num1 = 5;
+var num2 = 9;
+
+console.log('The sum of the numbers are :' + (num1 + num2));
+
+//line
+console.log('=====================');
