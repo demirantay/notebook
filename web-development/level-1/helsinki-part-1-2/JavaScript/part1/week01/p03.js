2db09bee81569f7bca18453fd5f6bcf3f6918904
Demir Antay <demir99antay@gmail.com>
2018-01-08 01:14:47 +0200

    addde more js to helsinki coruse

--- web-development/level-1/helsinki-part-1-2/JavaScript/part1/week01/p03.js
+++ web-development/level-1/helsinki-part-1-2/JavaScript/part1/week01/p03.js
@@ -9,3 +9,26 @@ if (number % 2 === 0) {
 } else {
   alert('number is odd');
 }
+
+//=============================================
+
+//exersice 17
+//passing this exersice because I have already done it earlier
+
+//===========================================
+
+//exersice 18
+
+var grade = parseInt(prompt('type your grade: '));
+
+if (grade >= 90) {
+  alert('A');
+} else if (grade >= 80) {
+  alert('B');
+} else if (grade >= 70) {
+  alert('C');
+} else if (grade >= 60) {
+  alert('D');
+} else {
+  alert('F');
+}
