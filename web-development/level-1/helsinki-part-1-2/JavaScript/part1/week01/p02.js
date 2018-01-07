8c160b85671bf0e3c8f7f689a67eff379b7fa732
Demir Antay <demir99antay@gmail.com>
2018-01-08 00:34:16 +0200

    Added more javascript practice

---
+++ web-development/level-1/helsinki-part-1-2/JavaScript/part1/week01/p02.js
@@ -0,0 +1,20 @@
+'user strict';
+
+//exersice 8
+//var number1 = parseInt(prompt('Type a number : '));
+//var number2 = parseInt(prompt('Another number: '));
+
+//alert('sum is :' + (number1 + number2));
+
+console.log('================================');
+
+//exersice 9
+
+var num1 = parseInt(prompt('Type a number: '));
+var num2 = parseInt(prompt('Type a number: '));
+
+alert(num1 / num2);
+
+console.log('================================');
+
+//exersice 10
