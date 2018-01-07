4535d22f57c0050ecfea5a1ccf0a52f307e8906c
Demir Antay <demir99antay@gmail.com>
2018-01-08 00:46:36 +0200

    added more js

--- web-development/level-1/helsinki-part-1-2/JavaScript/part1/week01/p02.js
+++ web-development/level-1/helsinki-part-1-2/JavaScript/part1/week01/p02.js
@@ -6,6 +6,7 @@
 
 //alert('sum is :' + (number1 + number2));
 
+/*
 console.log('================================');
 
 //exersice 9
@@ -18,3 +19,26 @@ alert(num1 / num2);
 console.log('================================');
 
 //exersice 10
+
+var radius = parseInt(prompt('Type radius: '));
+var circumfrence = radius * 3.1415 * 2;
+
+alert('Radius is, ' + circumfrence);
+
+console.log('================================');
+*/
+
+//exersice 11
+
+var num1 = parseInt(prompt('Type a number: '));
+var num2 = parseInt(prompt('Type a number: '));
+
+if (num1 > num2) {
+  alert('Number ' + num1 + ' is greater');
+}
+else if (num2 > num1) {
+  alert('number ' + num2 + ' is greater');
+}
+else {
+  alert('They are equal');
+}
