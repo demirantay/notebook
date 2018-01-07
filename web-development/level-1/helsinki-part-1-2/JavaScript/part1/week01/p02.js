65b61939d6149172bca5487e7055d17f12f7d6bf
Demir Antay <demir99antay@gmail.com>
2018-01-08 01:02:00 +0200

    added more js exersice in helsinki

--- web-development/level-1/helsinki-part-1-2/JavaScript/part1/week01/p02.js
+++ web-development/level-1/helsinki-part-1-2/JavaScript/part1/week01/p02.js
@@ -30,6 +30,7 @@ console.log('================================');
 
 //exersice 11
 
+/*
 var num1 = parseInt(prompt('Type a number: '));
 var num2 = parseInt(prompt('Type a number: '));
 
@@ -41,4 +42,30 @@ else if (num2 > num1) {
 }
 else {
   alert('They are equal');
+}
+
+console.log('==================================');
+
+//exersice 12
+
+var name1 = prompt('Type your name: ');
+var age1 = parseInt(prompt('Type your age: '));
+
+var name2 = prompt('Type another name: ');
+var age2 = parseInt(prompt('Type the age: '));
+
+alert(name1 + ' and ' + name2 + ' are ' + (age1 + age2) + ' age in total');
+
+console.log('=================================');
+
+*/
+
+//exersice 13
+
+var number = parseInt(prompt('Type a number : '));
+
+if (number >= 0 ) {
+  alert('number is positive');
+} else {
+  alert('number is negative');
 }
---
+++ web-development/level-1/helsinki-part-1-2/JavaScript/part1/week01/p03.js
@@ -0,0 +1,11 @@
+'user strict';
+
+//exersice 16
+
+var number = parseInt(prompt('type a number'));
+
+if (number % 2 === 0) {
+  alert('number is even');
+} else {
+  alert('number is odd');
+}
