641ab77a3c8140b9edeeca9abe7cae0b15828b79
Demir Antay <demir99antay@gmail.com>
2018-01-07 15:25:55 +0200

    added javascript practice

---
+++ web-development/level-1/helsinki-part-1-2/JavaScript/part1/week01/p01.js
@@ -0,0 +1,27 @@
+'use strict'
+
+//exersice 1
+
+console.log('Demir Antay');
+
+//line
+console.log('=====================');
+
+//exersice 2
+console.log('Hello World!');
+console.log('(and all the people of the world)');
+
+//line
+console.log('=====================');
+
+//exersice 3
+console.log('   *   ');
+console.log('  ***  ');
+console.log(' ***** ');
+console.log('*******');
+console.log('   *   ');
+
+//line
+console.log('=====================');
+
+//exersice 4
