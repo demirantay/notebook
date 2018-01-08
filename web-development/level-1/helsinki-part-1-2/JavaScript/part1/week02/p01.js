729c60c7fd1f1189bf01f21f51b28b7b1c3a8b1f
Demir Antay <demir99antay@gmail.com>
2018-01-08 22:39:31 +0200

    added more exersice to js in helsinki folder

---
+++ web-development/level-1/helsinki-part-1-2/JavaScript/part1/week02/p01.js
@@ -0,0 +1,62 @@
+/*
+// exersice 25
+
+var exersice_25 = function() {
+
+  var sum = 0;
+
+  for (i = 0; i < 3 ; i++){
+    var input_number = parseInt(prompt('Type a number: '));
+    sum += input_number;
+  }
+
+  alert('Sum is :'+ sum);
+}
+exersice_25();
+
+//------------------------------
+
+//exersice 26
+
+var sum = 0;
+
+while (true) {
+  var number = parseInt(prompt('Type a number: '));
+
+  if (number === 0) {
+    alert('sum in the end is '+ sum);
+    break;
+  }
+
+  sum += number;
+
+  alert('The sum is now: '+ sum);
+}
+
+//---------------------------------
+
+//exersice 27
+
+for (i = 0; i < 101; i++) {
+  console.log(i);
+}
+
+
+///--------------------------------
+
+//exersice 28
+
+for (i = 100; i > -1; i--) {
+  alert(i);
+}
+*/
+
+//--------------------------------
+
+//exersice 29
+
+for (i = 0; i < 101; i += 2) {
+  console.log(i);
+}
+
+//exersice 30
