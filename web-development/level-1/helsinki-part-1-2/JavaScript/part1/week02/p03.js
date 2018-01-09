a6a46a033f65c8a784eab0125a4e632d40f7237d
Demir Antay <demir99antay@gmail.com>
2018-01-09 21:18:48 +0200

    added more to helsinki js

---
+++ web-development/level-1/helsinki-part-1-2/JavaScript/part1/week02/p03.js
@@ -0,0 +1,37 @@
+//ex 35
+
+/*
+var sum = 1;
+
+var power = parseInt(prompt('Type a number: '));
+
+for (var i = 1; i <= power; i++) {
+  sum += Math.pow(2, i);
+}
+
+console.log(sum);
+*/
+
+//ex 36
+
+/*
+var sum = 0;
+var number_count = 0
+var average;
+
+while(true) {
+  var number = parseInt(prompt('Type numbers: '));
+  number_count++;
+  sum += number;
+  if (number === -1) {
+    console.log('Thank you and see you later');
+    console.log('The sum is : ' + (sum + 1));
+    console.log('You typed this many number: ' + (number_count - 1));
+    break;
+  }
+}
+
+console.log('Average : ' + (sum + 1 / number_count - 1));
+*/
+
+//ex 37
