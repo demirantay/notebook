1bdf659b91e019d1b610ddd778eb67f8eb82f1f7
Demir Antay <demir99antay@gmail.com>
2018-01-10 16:37:51 +0200

    added more js to helsinki

--- web-development/level-1/helsinki-part-1-2/JavaScript/part1/week02/p03.js
+++ web-development/level-1/helsinki-part-1-2/JavaScript/part1/week02/p03.js
@@ -32,6 +32,43 @@ while(true) {
 }
 
 console.log('Average : ' + (sum + 1 / number_count - 1));
-*/
+
 
 //ex 37
+
+var print_text= function(amount) {
+  for (i = 0; i < amount; i++) {
+    alert('In the begining there was swamp, joe and javascript');
+  }
+}
+
+var amount = parseInt(prompt('How many times: '));
+print_text(amount);
+
+
+*/
+
+
+//ex 39
+
+var print_stars = function(amount) {
+  var stars = '';
+  for (let i = 0; i < amount; i++) {
+    stars += '*';
+  }
+  document.write(stars + '<br>');
+}
+
+var print_square = function(side_size) {
+  for (var i = 0; i < side_size; i++) {
+    print_stars(4);
+  }
+}
+
+var print_rectangle = function(width, height) {
+  for (var i = 0; i < height; i++) {
+    print_stars(width);
+  }
+}
+
+print_triangle(5);
