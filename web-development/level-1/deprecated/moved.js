6655c7cb8bbb4a18124726f69d2839928323e824
Demir Antay <demir99antay@gmail.com>
2018-01-26 18:14:02 +0200

    more deprecated stuff

---
+++ web-development/level-1/deprecated/moved.js
@@ -0,0 +1,8 @@
+console.log('moved the helsinki javascript exersices');
+
+// I will no longer do the javascript helsinki exersices since I deleted the
+//file by a wrong decision and cannot take it back
+
+//but I have decided to dedicate myself learning javascript not python
+//since python is not the best 'tailored' language for the web I
+//will be sticking to javascript
