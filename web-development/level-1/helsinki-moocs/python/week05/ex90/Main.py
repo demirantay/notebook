049e01a53d2de27d87b4cbba9a314c6c923c7987
Demir Antay <demir99antay@gmail.com>
2018-02-19 16:45:15 +0200

    added more py notes to helsinki

---
+++ web-development/level-1/helsinki-moocs/python/week05/ex90/Main.py
@@ -0,0 +1,5 @@
+from Team import Team
+
+team_1 =  Team('Barcelona FC')
+
+print('Team: ', team_1.get_name())
---
+++ web-development/level-1/helsinki-moocs/python/week05/ex90/Team.py
@@ -0,0 +1,9 @@
+class Team:
+
+    #constructor
+    def __init__(self, name):
+        self.team_name = name
+
+    #behaviors
+    def get_name(self):
+        return self.team_name
