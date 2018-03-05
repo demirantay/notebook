e451ba4d76b3198b8115447b0ec490135b7f0f5d
Demir Antay <demir99antay@gmail.com>
2018-03-05 14:57:15 +0200

    changed names

--- web-development/level-1/compsci-intro-python/week01/problem2.py
+++
@@ -1,7 +0,0 @@
-user_input = input('Type random caharacters: ')
-amount = 0
-
-if 'bob' in user_input:
-    amount += 1
-
-print('Amount of bob appeard : ', amount)
--- web-development/level-1/compsci-intro-python/week01/problem3.py
+++
@@ -1,9 +0,0 @@
-user_input = input('Type chars: ')
-longest_alphabet_order = 0
-
-s = 'abdcasdasd'
-
-if s[0] == 'a':
-
-else:
-    print('There is no longest')
