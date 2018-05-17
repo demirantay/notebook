8002f0b0f1d548f131d03b10a5a82c72af143bdb
Demir Antay <demir99antay@gmail.com>
2018-05-17 21:25:51 +0300

    more java

---
+++ web-development/helsinki-mooc/java/ex46.java
@@ -0,0 +1,11 @@
+public class ex46 {
+
+  public static void main(String[] args) {
+    System.out.println("Averageof 1, 2, 3, 4 : " + average(1, 2, 3, 4));
+  }
+
+  public static double average(int num1, int num2, int num3, int num4) {
+    return (num1+num2+num3+num4) / 4;
+  }
+
+}
