d5c936fccad83143b4a3cf74bcf4460d5267ed9d
Demir Antay <demir99antay@gmail.com>
2018-06-09 20:47:31 +0300

    added more java to helsinki folder

---
+++ helsinki-mooc/java/ex84/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+  public static void main(String[] args) {
+    Counter counter = new Counter(2);
+
+    System.out.println(counter);
+    counter.decrease();
+    System.out.println(counter);
+    counter.decrease();
+    System.out.println(counter);
+    counter.decrease();
+    System.out.println(counter);
+    counter.decrease();
+    System.out.println(counter);
+    counter.decrease();
+
+  }
+}
