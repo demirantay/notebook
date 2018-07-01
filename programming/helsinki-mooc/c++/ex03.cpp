a44ad21d1ee8721fdb066520fc3e9de49a9dd9e8
Demir Antay <demir99antay@gmail.com>
2018-07-01 06:52:35 +0300

    tried out c and cpp

---
+++ programming/helsinki-mooc/c++/ex03.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+int main(int argc, char* argv[]) {
+  printf("*\n");
+  printf("**\n");
+  printf("***\n");
+  printf("****\n");
+  printf("*****\n");
+  printf("*\n");
+  return 0;
+}
---
+++ programming/helsinki-mooc/c/ex04.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+  int chicken = 10;
+  int bacon = 20;
+  char type[2] = "KG";
+
+  printf("Chicken: %d, Bacon: %d, \n", chicken, bacon );
+}
