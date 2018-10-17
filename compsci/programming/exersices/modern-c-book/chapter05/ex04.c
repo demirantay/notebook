9042f715c233cc5686df2267238168706ca90964
Demir Antay <demir99antay@gmail.com>
2018-10-17 20:45:51 +0300

    added more c exersices

--- compsci/programming/exersices/modern-c-book/chapter05/ex04.c
+++ compsci/programming/exersices/modern-c-book/chapter05/ex04.c
@@ -20,9 +20,47 @@ int main(void) {
   else if (first_year < second_year) {
     printf(
       "%d/%d/%d is erlier than %d/%d/%d \n",
-      first_date, first_month, first_year
-      second_date, second_month, second_year,
+      first_date, first_month, first_year,
+      second_date, second_month, second_year
     );
+  }
+  else if (first_year == second_year) {
+    if (first_month < second_month) {
+      printf(
+        "%d/%d/%d is erlier than %d/%d/%d \n",
+        first_date, first_month, first_year,
+        second_date, second_month, second_year
+      );
+    }
+    else if (second_month < first_month) {
+      printf(
+        "%d/%d/%d is erlier than %d/%d/%d \n",
+        second_date, second_month, second_year,
+        first_date, first_month, first_year
+      );
+    }
+    else if (first_month == second_month) {
+      if (first_date < second_date) {
+        printf(
+          "%d/%d/%d is erlier than %d/%d/%d \n",
+          first_date, first_month, first_year,
+          second_date, second_month, second_year
+        );
+      }
+      else if (second_date < first_date) {
+        printf(
+          "%d/%d/%d is erlier than %d/%d/%d \n",
+          second_date, second_month, second_year,
+          first_date, first_month, first_year
+        );
+      }
+    }
+    else {
+      printf("fuck off you entered wrong numbers!\n");
+    }
+  }
+  else {
+    printf("somthng went wrong \n");
   }
 
   return 0;
