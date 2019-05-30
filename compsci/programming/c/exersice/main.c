f1b502cc78b43f8add9fa01cf6ca8bd46e7727e9
Demir Antay <demir99antay@gmail.com>
2019-05-30 15:42:06 +0300

    Create main.c

---
+++ compsci/programming/c/exersice/main.c
@@ -0,0 +1,19 @@
+/*
+  This Project is a very basic money management system 
+  built with C. 
+  
+  author:
+  date:
+  github:
+  
+  Desc:
+*/
+
+
+#include <stdio.h>
+
+int main() {
+  
+
+  return 0;
+}
